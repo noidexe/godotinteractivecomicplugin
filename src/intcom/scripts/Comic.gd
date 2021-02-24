5d5e850d3c3c3076f3d33c02d1092895bdc21255
Lisandro Lorea <lisandrolorea@gmail.com>
2021-02-23 22:18:23 -0300

    subtransitions wip

--- src/intcom/scripts/Comic.gd
+++ src/intcom/scripts/Comic.gd
@@ -3,20 +3,29 @@ extends IC_Sequencer
 onready var transitions = $transitions
 var is_transitioning = false
 
+export(NodePath) var subtransition_target = NodePath()
+
 func show_next():
-	#if is_transitioning:
-	#	return
 	.show_next()
-	transitions.next()
-	#transitions.play(str(current))
+	if subtransition_target and get_node(subtransition_target).transitions.at_end:
+		subtransition_target = NodePath()
+	if not subtransition_target.is_empty() and not get_node(subtransition_target).transitions.at_end:
+		get_node(subtransition_target).show_next()
+	else:
+		transitions.next()
+	print(subtransition_target)
+
 	
 func show_previous():
-	#if is_transitioning:
-	#	return
-	#transitions.play_backwards(str(current))
-	transitions.back()
 	.show_previous()
-	
+	if subtransition_target and get_node(subtransition_target).transitions.at_start:
+		subtransition_target = NodePath()
+	if not subtransition_target.is_empty() and not get_node(subtransition_target).transitions.at_start:
+		get_node(subtransition_target).show_previous()
+	else:
+		transitions.back()
+	print(subtransition_target)
+
 
 func _on_transitions_animation_started(anim_name):
 	is_transitioning = true
@@ -36,3 +45,8 @@ func register_input(input_handler:IC_Input):
 
 func update_parallax(_motion:Vector2):
 	push_warning("Parallax motion unimplemented")
+
+
+func _on_transitions_frame_reached(cur, prev):
+	print(transitions.subtransitions.get(cur, NodePath()))
+	subtransition_target = transitions.subtransitions.get(cur, NodePath())
--- src/intcom/scripts/Transition.gd
+++ src/intcom/scripts/Transition.gd
@@ -7,16 +7,25 @@ var forward = true
 var is_transitioning = false
 export var transition_length := 0.4
 
+var at_start = true
+var at_end = false
+
 signal transition_started()
 signal transition_finished()
+signal frame_reached(cur, prev)
+
+var subtransitions = Dictionary()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	playback_process_mode = AnimationPlayer.ANIMATION_PROCESS_MANUAL
 	_fix_backwards_transition_length()
+	subtransitions = _get_subtransitions()
+	print(subtransitions)
 	pass # Replace with function body.
 
 func _process(delta):
+	var prev_frame = frame
 	# Calculate speedup
 	# If we are within the same frame it advances at normal speed
 	# If we are more than one frame ahead/behind, it advances by the square of that
@@ -27,31 +36,57 @@ func _process(delta):
 		advance(delta)
 		if current_animation_position > target_frame:
 			seek(target_frame, true)
+# warning-ignore:narrowing_conversion
 		frame = floor(current_animation_position)
 	else:
 		advance(-delta)
 		if current_animation_position < target_frame:
 			seek(target_frame, true)
+# warning-ignore:narrowing_conversion
 		frame = ceil(current_animation_position)
+	if frame != prev_frame:
+		emit_signal("frame_reached", frame, prev_frame)
 	_update_is_transitioning()
 	playback_speed = 1.0 / transition_length
 
 func next():
+# warning-ignore:narrowing_conversion
+	#target_frame = min(target_frame+1, int(current_animation_length))
+	if forward and is_transitioning:
+		seek(target_frame, true)
+		return
+	target_frame += 1
+	if target_frame > int(current_animation_length):
+		target_frame = int(current_animation_length)
+		seek(target_frame, true)
+		at_start = false
+		at_end = true
 	forward = true
-	target_frame = min(target_frame+1, int(current_animation_length))
-	pass
 	
 func back():
+# warning-ignore:narrowing_conversion
+	#target_frame = max(target_frame-1,0)
+	if not forward and is_transitioning:
+		seek(target_frame, true)
+		return
+	target_frame -= 1
+	if target_frame < 0:
+		target_frame = 0
+		seek(target_frame, true)
+		at_start = true
+		at_end = false
 	forward = false
-	target_frame = max(target_frame-1,0)
-	pass
 
 func _update_is_transitioning():
 	if is_transitioning and frame == target_frame:
 		is_transitioning = false
 		emit_signal("transition_finished")
+		at_start = frame == 0
+		at_end = frame == int(current_animation_length)
 	if not is_transitioning and frame != target_frame:
 		is_transitioning = true
+		at_start = false
+		at_end = false
 		emit_signal("transition_started")
 	
 func _fix_backwards_transition_length():
@@ -68,4 +103,16 @@ func _fix_backwards_transition_length():
 		backwards_keys.append([time,value])
 	for key in backwards_keys:
 		anim.track_insert_key(track,key[0],key[1])
-	print(backwards_keys)
+
+func _get_subtransitions() -> Dictionary:
+	var ret = Dictionary()
+	var anim : Animation = get_animation("transitions")
+	var track = anim.find_track(".:subtransition_target")
+	if track == -1:
+		return ret
+	var key_count = anim.track_get_key_count(track)
+	for i in key_count:
+		var time = int(anim.track_get_key_time(track, i))
+		var value = anim.track_get_key_value(track, i)
+		ret[time] = value
+	return ret
